0aac51a11b3914c7c48f0a6382d7bc5cde612bdb
jdlnn <jadeleeannsanchez@unomaha.edu>
2025-04-25 19:47:58 -0500

    First file

---
+++ television.py
@@ -0,0 +1,60 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self) -> None:
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+    def power(self) -> None:
+        self.__status = not self.__status
+        if self.__status:
+            print('TV is on')
+        else:
+            print('TV is off')
+
+    def mute(self) -> None:
+        if self.__status:
+            self.__muted = not self.__muted
+            if self.__muted:
+                self.__volume = Television.MIN_CHANNEL
+                print('TV is muted')
+            else:
+                print('TV is unmuted')
+
+
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.__status:
+            # self.__muted = False
+            if not self.__muted and self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self):
+        if self.__status:
+            # self.__muted = False
+            if not self.__muted and self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self) -> str:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+
+            # return f'Volume = {Television.MIN_VOLUME}'

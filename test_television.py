2fa96dfcc84adc4daa6271a541d670232d958833
jdlnn <jadeleeannsanchez@unomaha.edu>
2025-04-27 14:01:23 -0500

    First file

--- test_television.py
+++ test_television.py
@@ -9,62 +9,62 @@ class Test:
         del self.tv1
 
     def test_init(self):
-        assert self.tv1.__str__ == 'Power = False, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
 
     def test_power(self):
         self.tv1.power()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
         self.tv1.power()
-        assert self.tv1.__str__ == 'Power = False, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
 
     def test_mute(self):
         self.tv1.power()
         self.tv1.mute()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
         self.tv1.mute()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
         self.tv1.volume_up()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 1'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
 
         self.tv1.mute()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
     def test_channel_up(self):
         self.tv1.channel_up()
-        assert self.tv1.__str__ == 'Power = False, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
 
         self.tv1.power()
         self.tv1.channel_up()
-        assert self.tv1.__str__ == 'Power = True, Channel = 1, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 1, Volume = 0'
 
         self.tv1.channel_up()
         self.tv1.channel_up()
         self.tv1.channel_up()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
     def test_channel_down(self):
         self.tv1.power()
         self.tv1.channel_down()
-        assert self.tv1.__str__ == 'Power = True, Channel = 3, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 3, Volume = 0'
 
         self.tv1.channel_down()
         assert self.tv1.__str__ == 'Power = True, Channel = 2, Volume = 0'
 
         self.tv1.channel_down()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
     def test_volume_up(self):
         self.tv1.power()
         self.tv1.volume_up()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 1'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
 
         self.tv1.volume_up()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 2'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 2'
 
         self.tv1.volume_down()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 2'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 2'
 
 
     def test_volume_down(self):
@@ -72,11 +72,11 @@ class Test:
         self.tv1.volume_up()
         self.tv1.volume_up()
         self.tv1.volume_down()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 1'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
 
         self.tv1.volume_down()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
         self.tv1.volume_down()
-        assert self.tv1.__str__ == 'Power = True, Channel = 0, Volume = 0'
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
